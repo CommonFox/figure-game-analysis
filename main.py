359f78428a7f1ee5626b1ff012bfd65daa24d69f
commonfox <foxmaikovich@gmail.com>
2023-08-01 18:35:33 -0600

    properly compair for wins and losses

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def read_csv(fox_stats, brittany_stats):
     df_fox_stats = pd.read_csv(fox_stats)
@@ -9,6 +10,48 @@ def read_csv(fox_stats, brittany_stats):
 
     merged_df = pd.merge(df_fox_stats, df_brittany_stats, on=['Game #', 'Date'], suffixes=('_fox', '_brittany'))
     merged_df = merged_df.drop(['Player_fox', 'Player_brittany'], axis=1)
+
+    merged_df = tries_and_time_comparison(merged_df)
+
+    return merged_df
+
+def tries_and_time_comparison(merged_df):
+    df_copy = merged_df.copy()
+
+    df_copy['Tries_Comparison'] = np.nan
+    df_copy['Time_Comparison'] = np.nan
+
+    # Calculate the Tries_Comparison column based on different conditions
+    df_copy['Tries_Comparison'] = np.where(
+        (df_copy['Tries_fox'].notna()) & (df_copy['Tries_brittany'].notna()),   # Both not NaN
+        df_copy['Tries_fox'] - df_copy['Tries_brittany'],
+        np.nan  # Both are NaN
+    )
+
+    # Calculate the Time_Comparison column based on different conditions
+    df_copy['Time_Comparison'] = np.where(
+        (df_copy['Time_fox'].notna()) & (df_copy['Time_brittany'].notna()),   # Both not NaN
+        df_copy['Time_fox'] - df_copy['Time_brittany'],
+        np.nan  # Both are NaN
+    )
+
+    # Brittany plays but not Fox
+    mask = (df_copy['Tries_fox'].isna()) & (df_copy['Tries_brittany'].notna())
+    df_copy.loc[mask, 'Tries_Comparison'] = df_copy['Tries_brittany']
+
+    mask = (df_copy['Time_fox'].isna()) & (df_copy['Time_brittany'].notna())
+    df_copy.loc[mask, 'Time_Comparison'] = df_copy['Time_brittany']
+
+    # Fox plays but not Brittany
+    mask = (df_copy['Tries_fox'].notna()) & (df_copy['Tries_brittany'].isna())
+    df_copy.loc[mask, 'Tries_Comparison'] = df_copy['Tries_fox'] * -1
+
+    mask = (df_copy['Time_fox'].notna()) & (df_copy['Time_brittany'].isna())
+    df_copy.loc[mask, 'Time_Comparison'] = df_copy['Time_fox'] * -1
+
+    df_copy = df_copy[['Game #', 'Tries_Comparison', 'Time_Comparison']]
+
+    merged_df = pd.merge(merged_df, df_copy, on='Game #')
 
     return merged_df
 
